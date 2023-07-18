a92fdd7fc216949cf9b99d03c5dfcc106d0b3f80
AfzalRahmanCSE <123239866+AfzalRahmanCSE@users.noreply.github.com>
2023-07-18 22:20:20 +0530

    Basic product presentation done

--- src/product/ProductItem.js
+++ src/product/ProductItem.js
@@ -1,14 +1,23 @@
-const ProductItem=({product})=>{
-    const getDiscountedPrice=(product)=>{
-        return product.price-product.price*product.discount/100
+const ProductItem = ({ product }) => {
+    const getDiscountedPrice = (product) => {
+        return product.price - product.price * product.discount / 100
     }
-
     return <div>
-        <h1>{product.brand} {product.model}</h1>
-        <img src={product.image} width={100} height="100" alt=''/>
-        <h4 style={{textDecoration:product.discount>0?'line-through':''}}>Actual Price:{product.price}</h4>
-        <h4>Now:{getDiscountedPrice(product)}</h4>
-    </div>
+      
+
+                    <div className="card" style={{ margin: 10 }}>
+                        <img style={{ padding: 10 }} src={product.image} className="card-img-top" alt="..." width={200} height={300}/>
+                        <div className="card-body">
+                            <h5 className="card-title">{product.brand} {product.model}</h5>
+                            <p className="card-text"><h4 style={{ textDecoration: product.discount > 0 ? 'line-through' : '' }}>Actual Price:{product.price}</h4>
+                                <h4>Now:{getDiscountedPrice(product)}</h4></p>
+                            <button style={{ marginRight: 5 }} className="btn btn-secondary">Add to Cart<i className="fas fa-shopping-cart"></i></button>
+                            <button href="#" className="btn btn-primary" disabled={!product.inStock}>Buy Now</button>
+
+                        </div>
+                    </div>
+                </div>
+    
 }
 
 export default ProductItem
--- src/product/ProductList.js
+++ src/product/ProductList.js
@@ -1,15 +1,26 @@
 import ProductItem from "./ProductItem";
-const ProductList=()=>{
-    const products=[
-        {id:1,brand:'Apple',model:'iPhone 12 Pro',price:1200,discount:12,image:'https://s3no.cashify.in/pd-admin/149c1919c16b485ba860a2ea5ed9e793.jpg?p=es5sq&s=es'},
-        {id:2,brand:'Apple',model:'iPhone 13 Pro',price:1300,discount:13,image:'https://s3no.cashify.in/pd-admin/e250879f0b76406c883da271fe9ce730.jpg?p=es5sq&s=es'},
-        {id:3,brand:'Apple',model:'iPhone 14 Pro',price:1400,discount:14,image:'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-finish-select-202209-6-1inch-deeppurple?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1663703840578'},
+const ProductList = () => {
+    const products = [
+        { id: 1, brand: 'Apple', model: 'iPhone 12 Pro', price: 1200, discount: 12, inStock: true, image: 'https://s3no.cashify.in/pd-admin/149c1919c16b485ba860a2ea5ed9e793.jpg?p=es5sq&s=es' },
+        { id: 2, brand: 'Apple', model: 'iPhone 13 Pro', price: 1300, discount: 13, inStock: false, image: 'https://s3no.cashify.in/pd-admin/e250879f0b76406c883da271fe9ce730.jpg?p=es5sq&s=es' },
+        { id: 3, brand: 'Apple', model: 'iPhone 14 Pro', price: 1400, discount: 14, inStock: true, image: 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-finish-select-202209-6-1inch-deeppurple?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1663703840578' },
     ]
     return <div>
 
         <h1> Product List</h1>
-        {products.map(product =><ProductItem product={product}/>)}
-    </div> 
+        <div className="container">
+            <div className="row">
+               
+                    {products.map((product, index) => (
+                        <div key={index} className="col-md-4">
+                            <ProductItem product={product} />
+                        </div>
+                    ))}
+                
+            </div>
+        </div>
+    </div>
+
 
 }
 
