1f26066cdd384100f5ce19a44640c4a79ff2cc40
AfzalRahmanCSE <123239866+AfzalRahmanCSE@users.noreply.github.com>
2023-07-19 14:54:26 +0530

    All bugs Fixed for build

--- src/users/UserItem.js
+++ src/users/UserItem.js
@@ -1,21 +1,21 @@
 const UserItem=({user})=>{
     return <div>
-       <div class="card" style={{
+       <div className="card" style={{
           margin: 10,
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
           flexDirection: 'column',
         }}>
-  <img src={user.avatar_url} class="rounded-circle" alt="..."
+  <img src={user.avatar_url} className="rounded-circle" alt="..."
   style={{
     width: '100px', // Set the desired width of the image
     height: '100px',
   }}
   />
-  <div class="card-body">
+  <div className="card-body">
     <hr/>
-    <h5 class="card-title" style={{textAlign:'center'}}>{user.login}</h5>
+    <h5 className="card-title" style={{textAlign:'center'}}>{user.login}</h5>
     
   </div>
 </div>
